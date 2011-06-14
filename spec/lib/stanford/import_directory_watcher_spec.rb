75d726b764523411e729bc518cbcd065bc61b6fa
Chris Fitzpatrick <cfitz@stanford.edu>
2011-06-14 16:18:20 -0700

    Removing tmpdir gem

--- spec/lib/stanford/import_directory_watcher_spec.rb
+++ spec/lib/stanford/import_directory_watcher_spec.rb
@@ -1,6 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 require 'lib/stanford/import_directory_watcher'
-require 'Tmpdir'
 
 describe Stanford::ImportDirectoryWatcher do 
   
