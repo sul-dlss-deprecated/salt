be2adbf01532734465f81fd5997ace0fd273ca4a
Chris Beer <cabeer@stanford.edu>
2012-11-19 16:25:11 -0800

    fixup deploy

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -7,5 +7,3 @@ set :bundle_without, [:deployment]
 role :web, deployment_host
 role :app, deployment_host
 role :db,  deployment_host, :primary => true
-
-after "deploy", "db:migrate"
--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -9,4 +9,3 @@ role :web, deployment_host
 role :app, deployment_host
 role :db,  deployment_host, :primary => true
 
-after "deploy", "db:migrate"
