0e66d422ee2cc85689e3bb64f4d6490069eb7020
Chris Beer <cabeer@stanford.edu>
2012-11-26 15:57:34 -0800

    use rest-client instead of low-level net::http

--- lib/stanford/asset_repository.rb
+++ lib/stanford/asset_repository.rb
@@ -1,82 +1,46 @@
+require 'rest_client'
+
 module Stanford
   class AssetRepository
-    
-   
-  #
-  # This method initializes the asset repository
-    attr_reader :base
-    attr_reader :username
-    attr_reader :password
 
+   attr_reader :base
 
+   def initialize(base=nil)
 
-   def initialize(base=nil, username=nil, password=nil)
+     base = ASSET_SERVER_URI if base.nil?
 
-       base = ASSET_SERVER_URI if base.nil?
-       username = ASSET_SERVER_USER if username.nil?
-       password = ASSET_SERVER_PASSWORD if password.nil?
-       
-      base.chop! if /\/$/.match(base)
-      @base = base
-      @username = username
-      @password = password
+     base.chop! if /\/$/.match(base)
+     @base = base
     end
 
-     #       
+     #
      # This method gets the thumbnail for the asset form the asset repository. Thumbnails are named {druid}/thumb.jpg
     def get_thumbnail( druid )
-      uri = URI.parse(@base + "/" + druid + "/thumb.jpg"  )  
-      return Stanford::AssetRepository.http(uri,  @username, @password) 
-    rescue
-     return nil       
+      uri = @base + "/" + druid + "/thumb.jpg"
+      RestClient.get(uri).body rescue nil
     end
 
     # this method gets a PDF from the asset repository. PDF's are named {druid}/{druid}.pdf
     def get_pdf( druid )
-      uri = URI.parse("#{@base}/#{druid}/#{druid}.pdf")
-      return Stanford::AssetRepository.http(uri,  @username, @password) 
-    rescue  
-      return nil
+      uri = "#{@base}/#{druid}/#{druid}.pdf"
+      RestClient.get(uri).body rescue nil
     end
-    
+
     def get_json( druid )
-       uri = URI.parse("#{@base}/#{druid}/flipbook.json")
-       return Stanford::AssetRepository.http(uri,  @username, @password)  
+      uri = "#{@base}/#{druid}/flipbook.json"
+      RestClient.get(uri).body rescue nil
     end
-    
-    # gets a page jp2000 from the asset repo. JP2000s are named {druid}/{druid}_{page number}.jp2. Page numbers are 5 digit format (00004). 
+
+    # gets a page jp2000 from the asset repo. JP2000s are named {druid}/{druid}_{page number}.jp2. Page numbers are 5 digit format (00004).
     def get_page_jp2(druid, page_number)
-      uri = URI.parse(@base + "/" + druid + "/#{druid}_#{page_number}.jp2")
-      return Stanford::AssetRepository.http(uri,  @username, @password) 
-    rescue
-         return nil
+      uri = @base + "/" + druid + "/#{druid}_#{page_number}.jp2"
+      RestClient.get(uri).body rescue nil
     end
-    
-    # gets a page Alto XML from the asset repo. XML files are named {druid}/{druid}_{page number}.xml. Page numbers are 5 digit format (00004). 
+
+    # gets a page Alto XML from the asset repo. XML files are named {druid}/{druid}_{page number}.xml. Page numbers are 5 digit format (00004).
     def get_page_xml(druid, page_number)
-       uri = URI.parse(@base + "/" + druid + "/#{druid}_#{page_number}.xml"  )    
-      return Stanford::AssetRepository.http(uri, @username, @password) 
-    rescue
-       return nil
+      uri = @base + "/" + druid + "/#{druid}_#{page_number}.xml"
+      RestClient.get(uri).body rescue nil
     end
-
-private
-
-     def self.http(uri, username, password, limit = 10)
-       request = Net::HTTP::Get.new(uri.request_uri) 
-       request.basic_auth username, password
-       response = Net::HTTP.start(uri.host, uri.port) {|http| http.request(request)}
-       case response
-       when Net::HTTPSuccess then response.body
-       when Net::HTTPRedirection then Stanford::AssetRepository.http(response['location'], limit - 1)
-       else
-         raise response.error!
-       end
-       rescue Exception => e
-             raise e
-     end #rescue 
-    
-    
-    
   end
 end
--- spec/lib/stanford/asset_repository_spec.rb
+++ spec/lib/stanford/asset_repository_spec.rb
@@ -3,49 +3,38 @@ require 'fakeweb'
 
 describe Stanford::AssetRepository do
   before(:all) do
-    # This describe the layout of the assets on the webserver: 
+    # This describe the layout of the assets on the webserver:
     @druid = "ff12345"
-    @page = "000001" 
+    @page = "000001"
     #thumbnail
-    
-    FakeWeb.register_uri(:get,"http://fedoraAdmin:fedoraAdmin@example.com/#{@druid}/thumb.jpg" , :body => "A thumbnail") 
-    #pdf
-    pdf = "http://fedoraAdmin:fedoraAdmin@example.com/ff12345/ff12345.pdf"
-    FakeWeb.register_uri(:get, pdf , :body => "A pdf") 
-    #a page jp2000
-    FakeWeb.register_uri(:get, "http://fedoraAdmin:fedoraAdmin@example.com/#{@druid}/#{@druid}_#{@page}.jp2", :body => "A jp2000") 
-    #a page ALTO XML OCR Text file
-    FakeWeb.register_uri(:get, "http://fedoraAdmin:fedoraAdmin@example.com/#{@druid}/#{@druid}_#{@page}.xml", :body => "An alto file") 
-    
-    
-    
+
   end
-  
-  
+
+
   before(:each) do
-    @asset_repo = Stanford::AssetRepository.new("http://example.com", "fedoraAdmin", "fedoraAdmin")
+    @asset_repo = Stanford::AssetRepository.new("http://fedoraAdmin:fedoraAdmin@example.com/assets")
   end
-  
+
   it "should get the thumbnail for an asset but return nil for one that doesn't exist" do
-     
-     @asset_repo.get_thumbnail(@druid).should == "A thumbnail"
-     @asset_repo.get_thumbnail("bs").should be_nil
+    RestClient.expects(:get).with("http://fedoraAdmin:fedoraAdmin@example.com/assets/#{@druid}/thumb.jpg").returns(mock(:body => '123'))
+    @asset_repo.get_thumbnail(@druid).should == '123'
   end
-  
-  it "should get the pdf for an asset" do 
-    @asset_repo.get_pdf(@druid).should == "A pdf"
-    @asset_repo.get_pdf("bs").should be_nil
+
+  it "should get the pdf for an asset" do
+    RestClient.expects(:get).with("http://fedoraAdmin:fedoraAdmin@example.com/assets/#{@druid}/#{@druid}.pdf").returns(mock(:body => '123'))
+    @asset_repo.get_pdf(@druid).should == '123'
   end
-  
+
   it "should get the jp2000 for a page of an asset" do
-    @asset_repo.get_page_jp2(@druid, @page).should == "A jp2000"
-    @asset_repo.get_page_jp2("bs", "more bs").should be_nil
-    
+    RestClient.expects(:get).with("http://fedoraAdmin:fedoraAdmin@example.com/assets/#{@druid}/#{@druid}_#{@page}.jp2").returns(mock(:body => '123'))
+
+    @asset_repo.get_page_jp2(@druid, @page).should == '123'
   end
-  
+
   it "should get the alto for a page of an asset" do
-    @asset_repo.get_page_xml(@druid, @page).should == "An alto file"
-    @asset_repo.get_page_xml("bs", "more bs").should be_nil
+    RestClient.expects(:get).with("http://fedoraAdmin:fedoraAdmin@example.com/assets/#{@druid}/#{@druid}_#{@page}.xml").returns(mock(:body => '123'))
+
+    @asset_repo.get_page_xml(@druid, @page).should == '123'
   end
-  
+
 end
