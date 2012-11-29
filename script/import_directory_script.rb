70b293b15943400976925b034ebb03b9ce599dc9
Chris Beer <cabeer@stanford.edu>
2012-11-28 21:06:22 -0800

    use DIRECTORY_WATCHER_DIR in the import directory script

--- script/import_directory_script.rb
+++ script/import_directory_script.rb
@@ -8,7 +8,7 @@ require File.expand_path("../../config/environment", __FILE__)
 class ImportDirectoryScript
 
   REMOTE_DIR = AFS_DIRECTORY_WATCHER_DIR
-  LOCAL_DIR = File.join(Rails.root, 'tmp')
+  LOCAL_DIR = DIRECTORY_WATCHER_DIR
 
   @logger = Logger.new(File.join(Rails.root, 'log', 'import_directory.log'))
   @logger.level = Logger::INFO
