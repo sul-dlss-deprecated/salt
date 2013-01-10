005930017224acb3d73c1f38708d44124d4187f6
Chris Beer <cabeer@stanford.edu>
2013-01-10 07:01:35 -0800

    move download PDF link from flipbook to catalog#show

--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -4,11 +4,9 @@ module AssetHelper
  # returns the flipbook url
   def flipbook_tag(id)
       flipbook_url = "#{url_for(:action => 'show', :controller => 'asset', :id => id, :format => :flipbook )}"
-      pdf_url = "#{url_for(:action => 'show', :controller => 'asset', :id => id, :format => :pdf)}"
       iframe =  "<iframe src='#{flipbook_url}' width='99%' height='450px'/>"
       flipbook_link = "<a href='#{flipbook_url}' style='cursor:pointer;' onclick=\"window.open('#{flipbook_url}','status=0','toolbar=0','location=0','menubar=0','directories=0','navigation=0');return false;\">Open viewer in new window</a>"
-      pdf_link = "<a href='#{pdf_url}' style='cursor:pointer;' onclick=\"window.open('#{pdf_url}], 'status=0', 'toolbar=0', 'location=0', 'menubar=0', 'directories=0', 'navigation=0'); return false;\">Download PDF</a>"
-      return "#{iframe}#{flipbook_link}<br/>#{pdf_link}".html_safe
+      return "#{iframe}#{flipbook_link}".html_safe
   end
   
   
--- spec/helpers/asset_helper_spec.rb
+++ spec/helpers/asset_helper_spec.rb
@@ -6,7 +6,7 @@ describe AssetHelper do
   
 
   it "should return the proper url based on the asset ID" do
-    helper.flipbook_tag("foobar").should ==  "<iframe src='/assets/foobar.flipbook' width='99%' height='450px'/><a href='/assets/foobar.flipbook' style='cursor:pointer;' onclick=\"window.open('/assets/foobar.flipbook','status=0','toolbar=0','location=0','menubar=0','directories=0','navigation=0');return false;\">Open viewer in new window</a><br/><a href='/assets/foobar.pdf' style='cursor:pointer;' onclick=\"window.open('/assets/foobar.pdf], 'status=0', 'toolbar=0', 'location=0', 'menubar=0', 'directories=0', 'navigation=0'); return false;\">Download PDF</a>"
+    helper.flipbook_tag("foobar").should ==  "<iframe src='/assets/foobar.flipbook' width='99%' height='450px'/><a href='/assets/foobar.flipbook' style='cursor:pointer;' onclick=\"window.open('/assets/foobar.flipbook','status=0','toolbar=0','location=0','menubar=0','directories=0','navigation=0');return false;\">Open viewer in new window</a>"
   end
   
   
