b59bb439024b1c25ffec2526924f7e04c4af68eb
Chris Beer <cabeer@stanford.edu>
2015-06-26 10:19:45 -0700

    Update capistrano/passenger tasks to restart after deploy

--- Capfile
+++ Capfile
@@ -5,6 +5,7 @@ require 'capistrano/setup'
 require 'capistrano/deploy'
 
 require 'capistrano/rails'
+require 'capistrano/passenger'
 require 'dlss/capistrano'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
--- config/deploy.rb
+++ config/deploy.rb
@@ -41,28 +41,7 @@ set :linked_dirs, %w{bin config/settings log tmp/pids tmp/cache tmp/sockets vend
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
 
 namespace :salt do
   task :watcher do
